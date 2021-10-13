640369d0dcec0bbd1db3f6549c317d24a1f2ee9a
RAFAEL PEREIRA BOTELHO <88045307+faelpbt@users.noreply.github.com>
2021-10-13 17:16:10 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+
+import './index.css'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name:'Home', component: Home },
+        { path: '/About', name:'About', component: About }
+    ]
+})
+
+createApp(App)
+.use(router)
+.mount('#app')
